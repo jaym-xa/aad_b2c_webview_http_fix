b35f04db3a1d031132ea53e51a0e8003719f85e3
ragulnathan <ragul.boopathi@xagroup.com>
2024-07-23 16:21:15 +0530

    url issue fix

--- lib/src/presentation/aad_b2c_webview.dart
+++ lib/src/presentation/aad_b2c_webview.dart
@@ -103,6 +103,8 @@ class ADB2CEmbedWebViewState extends State<ADB2CEmbedWebView> {
             setState(() {
               isLoading = false;
             });
+            final Uri response = Uri.dataFromString(url);
+            onPageFinishedTasks(url, response);
           },
         ),
       )
